0948ca365484879a9ea6c165f6d47cd4a5c57f1b
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-11-21 20:46:28 -0800

    cookie parser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "pug");
 
@@ -19,7 +21,7 @@ app.get("/cards", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-	res.render("hello");
+	res.render("hello", { name: req.cookies.username });
 });
 
 app.post("/hello", (req, res) => {

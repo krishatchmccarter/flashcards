1adfdc2a9236c37bea6fc041dab7fe700e7fa72d
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-11-28 17:38:16 -0800

    router1

--- app.js
+++ app.js
@@ -9,53 +9,6 @@ app.use(cookieParser());
 
 app.set("view engine", "pug");
 
-// app.use((req, res, next) => {
-// 	console.log("Hello");
-// 	const err = new Error("oh noes");
-// 	err.status = 500;
-// 	next(err);
-// });
-
-app.use((req, res, next) => {
-	console.log("world");
-	next();
-});
-
-app.get("/", (req, res) => {
-	const name = req.cookies.username;
-	if (name) {
-		res.render("index", { name });
-	} else {
-		res.redirect("hello");
-	}
-});
-
-app.get("/cards", (req, res) => {
-	res.render("card", {
-		prompt: "Who is buried in Grant's tomb",
-		hint: "Think about whose tomb it is",
-	});
-});
-
-app.get("/hello", (req, res) => {
-	const name = req.cookies.username;
-	if (name) {
-		res.redirect("/");
-	} else {
-		res.render("hello");
-	}
-});
-
-app.post("/hello", (req, res) => {
-	res.cookie("username", req.body.username);
-	res.redirect("/");
-});
-
-app.post("/goodbye", (req, res) => {
-	res.clearCookie("username");
-	res.redirect("/hello");
-});
-
 app.use((req, res, next) => {
 	const err = new Error("Not Found");
 	err.status = 404;

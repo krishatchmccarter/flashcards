e8e438a23a18ee066f056e47e7730f5dc9b82f59
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-11-28 20:07:15 -0800

    query string

--- routes/cards.js
+++ routes/cards.js
@@ -6,10 +6,10 @@ const { cards } = data; //cards = cards.data
 router.get("/:id", (req, res) => {
 	const { side } = req.query;
 	const { id } = req.params;
-	const { text } = cards[id][side];
+	const text = cards[id][side];
 	const { hint } = cards[id];
 
-	const { templateData } = { text, hint };
+	const templateData = { text, hint };
 	res.render("card", templateData);
 });
 

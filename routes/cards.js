26137cb6122f0bb5761fd4984cadba35ff689b79
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-11-28 19:55:05 -0800

    url

--- routes/cards.js
+++ routes/cards.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const { data } = require("../data/flashcardData.json");
 const { cards } = data; //cards = cards.data
 
-router.get("/", (req, res) => {
+router.get("/:id", (req, res) => {
+	const { side } = req.query;
+	const { id } = req.params;
+	const { text } = cards[id][side];
+	const { hint } = cards[id];
+
+	const { templateData } = { text, hint };
+
 	res.render("card", {
-		prompt: cards[0].question,
-		hint: cards[0].hint,
+		prompt: cards[req.params.id].question,
+		hint: cards[req.params.id].hint, //using id in params because that's what we called it in url
 	});
 });
 

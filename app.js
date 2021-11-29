56b010bf2bbac9adbabafe0c305129b0d710e526
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-11-28 17:49:37 -0800

    card router

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(cookieParser());
 
 app.set("view engine", "pug");
 
+const mainRoutes = require("./routes");
+const cardRoutes = require("./routes/cards");
+
+app.use(mainRoutes);
+app.use("/cards", cardRoutes);
+
 app.use((req, res, next) => {
 	const err = new Error("Not Found");
 	err.status = 404;
